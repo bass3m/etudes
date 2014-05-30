27233b5b59305b1721da92ebc6af38b803753765
Bassem Youssef <bassem@outlook.com>
2014-05-30 08:11:32 -0700

    More basic etudes

--- lib/geom.ex
+++ lib/geom.ex
@@ -8,4 +8,13 @@ defmodule Geom do
   def area(h \\ 1,w \\ 1) do
     h * w
   end
+  def area(:rectangle,h,w) do
+    h * w
+  end
+  def area(:triangle,b,h) do
+    (b * h)/2
+  end
+  def area(:ellipse,a,b) do
+    :math.pi() * a * b
+  end
 end
