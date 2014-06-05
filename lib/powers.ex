c1683ca95dedd80fdd11657368c0cc57414f8734
Bassem Youssef <bassem@outlook.com>
2014-06-04 21:00:33 -0700

    Powers

--- lib/powers.ex
+++ lib/powers.ex
@@ -1,5 +1,5 @@
 defmodule Powers do
-  import Kernel, except:  [raise: 2]
+  import Kernel, except:  [raise: 2, raise: 3]
 
   def raise(_x,0), do: 1
   def raise(x,1), do: x
@@ -7,6 +7,11 @@ defmodule Powers do
     1/raise(x,-n)
   end
   def raise(x,n) do
-    x * raise(x,n-1)
+    raise(x,n,1)
+  end
+
+  def raise(_x,0,acc), do: acc
+  def raise(x,n,acc) do
+    raise(x,n-1,acc*x)
   end
 end
