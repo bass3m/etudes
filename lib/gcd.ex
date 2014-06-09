5d6c53ff542cca847096b662dee5a06f81272a65
Bassem Youssef <bassem@outlook.com>
2014-06-09 07:36:33 -0700

    Fixed examples

--- lib/gcd.ex
+++ lib/gcd.ex
@@ -1,13 +1,13 @@
 defmodule Gcd do
   @moduledoc """
   Calculate the GCD using Djikstra's method
-  iex(2)> Dijkstra.gcd(2, 8)
+  iex(2)> Gcd.gcd(2, 8)
   2
-  iex(3)> Dijkstra.gcd(14, 21)
+  iex(3)> Gcd.gcd(14, 21)
   7
-  iex(4)> Dijkstra.gcd(125, 46)
+  iex(4)> Gcd.gcd(125, 46)
   1
-  iex(5)> Dijkstra.gcd(120, 36)
+  iex(5)> Gcd.gcd(120, 36)
   12
   """
   def gcd(m,n) when m == n, do: m
