3107115189f69b5d00c91ab4c319cba95ab19432
Bassem Youssef <bassem@outlook.com>
2014-06-14 08:35:46 -0700

    Add a couple of card utils

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,8 +1,20 @@
 defmodule Cards do
+  @ranks ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+
   def make_deck() do
-    ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
     suites = ["Hearts","Diamonds","Clubs","Spades"]
-    for r <- ranks, s <- suites, do: {r,s}
+    for r <- @ranks, s <- suites, do: {r,s}
+  end
+
+  defp card_index({rank,_} = card) do
+    @ranks
+    |> Enum.with_index
+    |> Enum.find(fn({r,index})-> r == rank end)
+    |> elem(1)
+  end
+
+  def card_compare(card1,card2) do
+    card_index(card1) > card_index(card2)
   end
 
   @doc """
