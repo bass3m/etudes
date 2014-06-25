bd9a6d8b75a39e123fc8397628f9c76d1bd5d0b7
Bassem Youssef <bassem@outlook.com>
2014-06-25 08:21:07 -0700

    fix atomics

--- lib/atomic_maker.ex
+++ lib/atomic_maker.ex
@@ -2,24 +2,11 @@ defmodule AtomicMaker do
   @moduledoc """
   Macros that return the atomic weight of an element
   """
-  defmacro generate_atomic_weights() do
-    quote do
-      atomics = [{h, 1.008}, {he, 4.003},
-                 {li, 6.94}, {be, 9.012}, {b, 10.81}, {c, 12.011},
-                 {n, 14.007}, {o, 15.999}, {f, 18.998}, {ne, 20.178},
-                 {na, 22.990}, {mg, 24.305}, {al, 26.981}, {si, 28.085},
-                 {p, 30.974}, {s, 32.06}, {cl, 35.45}, {ar, 39.948},
-                 {k, 39.098}, {ca, 40.078}, {sc, 44.956}, {ti, 47.867},
-                 {v, 50.942}, {cr, 51.996}, {mn, 54.938}, {fe, 55.845}]
-      for {atom, weight} <- atomics do
-        quote do def unquote(atom)(), do: unquote(weight) end
-        #def atom(), do: weight
+  defmacro generate_atomic_weights(weights) do
+    for {atom, weight} <- weights do
+      quote do
+          def unquote(atom), do: unquote(weight)
       end
     end
   end
-
-  #defmacro h() do
-    #1.008
-  #end
-
 end
