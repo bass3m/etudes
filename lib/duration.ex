314683049139b2934f0eccba4f67197735e7a3f0
Bassem Youssef <bassem@outlook.com>
2014-06-28 17:34:58 -0700

    Duration minor changes

--- lib/duration.ex
+++ lib/duration.ex
@@ -13,4 +13,15 @@ defmodule Duration do
       end
     end
   end
+
+  defmacro {m1,s1} + {m2,s2} do
+    quote do
+      total_secs = unquote(s1) + unquote(s2)
+      if total_secs > 59 do
+        {unquote(m1) + unquote(m2) + 1, rem(total_secs,60)}
+      else
+        {unquote(m1) + unquote(m2),total_secs}
+      end
+    end
+  end
 end
