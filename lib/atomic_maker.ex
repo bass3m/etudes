8b339a8ab3ad652250b8b034496ab2560fe6e497
bass3m <bassem@outlook.com>
2014-07-07 21:44:44 -0700

    Update atomic_maker.ex

--- lib/atomic_maker.ex
+++ lib/atomic_maker.ex
@@ -1,6 +1,18 @@
 defmodule AtomicMaker do
   @moduledoc """
   Macros that return the atomic weight of an element
+  iex(1)> c("atomic_maker.ex")
+  [AtomicMaker]
+  iex(2)> c("atomic.ex")
+  [Atomic]
+  iex(3)> import Atomic
+  nil
+  iex(4)> water = h * 2 + o
+  18.015
+  iex(5)> sulfuric_acid = h * 2 + s + o * 4
+  98.072
+  iex(6)> salt = na + cl
+  58.44
   """
   defmacro generate_atomic_weights(weights) do
     for {atom, weight} <- weights do
